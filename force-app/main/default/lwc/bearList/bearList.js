b8ac54647aa2a7900b44bb5c3e46416c4a1b8165
jjrodriguez <jjrodriguez@seidor.com>
2024-01-10 16:33:16 +0100

    home map to see bears in search

--- force-app/main/default/lwc/bearList/bearList.js
+++ force-app/main/default/lwc/bearList/bearList.js
@@ -1,38 +1,58 @@
-import { NavigationMixin } from 'lightning/navigation';
-import { LightningElement, wire } from 'lwc';
+import { publish, MessageContext } from "lightning/messageService";
+import BEAR_LIST_UPDATE_MESSAGE from "@salesforce/messageChannel/BearListUpdate__c";
+import { NavigationMixin } from "lightning/navigation";
+import { LightningElement, wire } from "lwc";
 /** BearController.searchBears(searchTerm) Apex method */
-import searchBears from '@salesforce/apex/BearController.searchBears';
+import searchBears from "@salesforce/apex/BearController.searchBears";
 export default class BearList extends NavigationMixin(LightningElement) {
-	searchTerm = '';
-	@wire(searchBears, {searchTerm: '$searchTerm'})
-	bears;
-	handleSearchTermChange(event) {
-		// Debouncing this method: do not update the reactive property as
-		// long as this function is being called within a delay of 300 ms.
-		// This is to avoid a very large number of Apex method calls.
-		window.clearTimeout(this.delayTimeout);
-		const searchTerm = event.target.value;
-		// eslint-disable-next-line @lwc/lwc/no-async-operation
-		this.delayTimeout = setTimeout(() => {
-			this.searchTerm = searchTerm;
-		}, 300);
-	}
-	get hasResults() {
-		return (this.bears.data.length > 0);
-	}
-	handleBearView(event) {
-		// Get bear record id from bearview event
-		const bearId = event.detail;
-		// Navigate to bear record page
-		this[NavigationMixin.Navigate]({
-			type: 'standard__recordPage',
-			attributes: {
-				recordId: bearId,
-				objectApiName: 'Bear__c',
-				actionName: 'view',
-			},
-		});
-	}
+  searchTerm = "";
+  bears;
+
+/*Code highlights:
+We retrieve the Lightning message context and store it in a messageContext property.
+We use a wired function to capture incoming bear list data and fire a custom BearListUpdate__c Lightning message with the list of bear records.
+We pass searchTerm as a dynamic parameter to our wired searchBears adapter so that each time searchTerm changes, loadBears is re-executed and we fire a new message with the new search results.
+We use the publish function that we imported from LMS to fire a BearListUpdate__c Lightning message with the bear list.
+*/
+
+  @wire(MessageContext) messageContext;
+  @wire(searchBears, { searchTerm: "$searchTerm" })
+  loadBears(result) {
+    this.bears = result;
+    if (result.data) {
+      const message = {
+        bears: result.data,
+      };
+      publish(this.messageContext, BEAR_LIST_UPDATE_MESSAGE, message);
+    }
+  }
+  handleSearchTermChange(event) {
+    // Debouncing this method: do not update the reactive property as
+    // long as this function is being called within a delay of 300 ms.
+    // This is to avoid a very large number of Apex method calls.
+    window.clearTimeout(this.delayTimeout);
+    const searchTerm = event.target.value;
+    // eslint-disable-next-line @lwc/lwc/no-async-operation
+    this.delayTimeout = setTimeout(() => {
+      this.searchTerm = searchTerm;
+    }, 300);
+  }
+  get hasResults() {
+    return this.bears.data.length > 0;
+  }
+  handleBearView(event) {
+    // Get bear record id from bearview event
+    const bearId = event.detail;
+    // Navigate to bear record page
+    this[NavigationMixin.Navigate]({
+      type: "standard__recordPage",
+      attributes: {
+        recordId: bearId,
+        objectApiName: "Bear__c",
+        actionName: "view",
+      },
+    });
+  }
 }
 // import { LightningElement } from 'lwc';
 // import ursusResources from '@salesforce/resourceUrl/ursus_park';
